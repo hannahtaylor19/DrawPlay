b0263c5afbf2b2e161d6e8e202ef82b5cd583965
Hayden Cooreman <hcooreman@mines.edu>
2021-09-28 14:31:14 -0600

    Modified cat's meow to include more wwws, changed the frame size for the graphic, and modified the size of the cat's head. Additional comments have been added in the appropriate placed.

--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -14,7 +14,8 @@ public class MyDrawing extends JPanel {
 	// paintComponent is called automatically when the frame needs
 	// to display (e.g., when the program starts)
 	public void paintComponent(Graphics g) {
-		cat.draw(g, 50, 50);
+		// size modified to 55
+		cat.draw(g, 55, 55);
 	}
 	
 	/**
@@ -27,7 +28,9 @@ public class MyDrawing extends JPanel {
 		// Program will end when the window is closed
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// Set the size (otherwise only title bar displays)
-		frame.setSize(400, 400);
+		
+		// size modified from option 1 commit
+		frame.setSize(500, 500);
 		
 		// Frame will not display until you set visible true
 		frame.setVisible(true);
